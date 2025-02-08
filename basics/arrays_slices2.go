4a8f22b0e2114c02a2bc86d828c572c60b98fd3d
A7mod <aamodkrishna@gmail.com>
2025-02-08 20:45:42 +0530

    Added Arrays & Slices 2

---
+++ basics/arrays_slices2.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	slice := []int{1, 2, 3, 4, 5} // slice declaration (this isan array tho!)
+	fmt.Println("Slice:", slice)
+
+	// append elements in that array.
+	slice = append(slice, 6, 7, 8)
+	fmt.Println("After Append:", slice)
+
+	arr := []int{10, 20, 30, 40, 50, 60, 70}
+	fmt.Println("Array before:", arr)
+	slice2 := arr[1:4]
+	slice3 := slice2[1:5] // interesting revelation this slice3 still pulls items from arr and not slice3.
+	fmt.Println("Sliced Array slice2:", slice2)
+	fmt.Println("Sliced Array slice3:", slice3)
+}
