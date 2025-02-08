7c57c4bcf1785985276f569d7ed7d3b0e8b0172a
A7mod <aamodkrishna@gmail.com>
2025-02-08 18:22:06 +0530

    Added loops yo

---
+++ basics/loops.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// For loop
+	for i := 1; i <= 5; i++ {
+		fmt.Println("Count:", i)
+	}
+
+	// For range
+	numbers := []int{10, 20, 30, 40, 50}
+	for index, value := range numbers {
+		fmt.Println("Index:", index, "value:", value)
+	}
+}
