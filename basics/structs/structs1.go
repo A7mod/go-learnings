59db371dea638965b103ccf5c1174fc5b07061b8
A7mod <aamodkrishna@gmail.com>
2025-02-11 01:04:55 +0530

    Rearranged the main.go file. Also started structs

--- basics/structs/structs1.go
+++ basics/structs/structs1.go
@@ -1,1 +1,19 @@
 package structs
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func StructBasics() {
+	p := Person{"Amit", 25} // this is a struct instance
+	fmt.Println("This is the struct:", p)
+
+	// Accessing items:
+	fmt.Println("Name:", p.Name)
+	fmt.Println("Age:", p.Age)
+	fmt.Println("----------------------------")
+
+}
--- main.go
+++ main.go
@@ -7,36 +7,45 @@ import (
 	"github.com/A7mod/go-learnings/basics/arrays"
 	"github.com/A7mod/go-learnings/basics/functions"
 	"github.com/A7mod/go-learnings/basics/maps"
+	"github.com/A7mod/go-learnings/basics/structs"
 )
 
 func main() {
 	fmt.Println("Running Basics ... ")
 
 	// basics i.e. main folder-ch
+	fmt.Println("\n--- Basics ---")
 	basics.ShowVariables()
 	basics.ShowLoops()
 	basics.CheckCondition()
 
 	// arrays vale items
+	fmt.Println("\n--- Arrays ---")
 	arrays.ShowArrays()
 	arrays.ShowSlices()
 	arrays.ShowSlices2()
 
 	// functions vale items
+	fmt.Println("\n--- Functions ---")
+	functions.ShowFunctions()
 	functions.Represent()
 	functions.NamedReturns()
 	functions.ShowAnonfunc()
 	functions.ShowVariadicFunc()
-	functions.ShowFunctions()
 	functions.ShowFuncAsArgument()
 	functions.ShowClosures()
 	functions.ShowMethods()
 
 	// maps vaale items
+	fmt.Println("\n--- Maps ---")
 	maps.ShowMaps()
 	maps.ModifyMaps()
 	maps.CheckKeyExists()
 	maps.IterateMaps()
 	maps.ShowNests()
+
+	// Structs ka maamla
+	fmt.Println("\n--- Structs ---")
+	structs.StructBasics()
 
 }
