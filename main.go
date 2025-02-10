cf56d2977f59e72199fe112e16b0b73df32c1f66
A7mod <aamodkrishna@gmail.com>
2025-02-10 23:02:07 +0530

    Ek rearrangement ki vajah se itne changes !

--- main.go
+++ main.go
@@ -4,27 +4,35 @@ import (
 	"fmt"
 
 	"github.com/A7mod/go-learnings/basics"
+	"github.com/A7mod/go-learnings/basics/arrays"
+	"github.com/A7mod/go-learnings/basics/functions"
 	"github.com/A7mod/go-learnings/basics/maps"
 )
 
 func main() {
 	fmt.Println("Running Basics ... ")
+
+	// basics i.e. main folder-ch
 	basics.ShowVariables()
 	basics.ShowLoops()
-	basics.ShowFunctions()
 	basics.CheckCondition()
-	basics.ShowArrays()
-	basics.ShowSlices()
-	basics.ShowSlices2()
-	basics.Represent()
-	basics.NamedReturns()
-	basics.ShowAnonfunc()
-	basics.ShowVariadicFunc()
-	basics.ShowFuncAsArgument()
-	basics.ShowClosures()
-	basics.ShowMethods()
 
-	//maps vaale items
+	// arrays vale items
+	arrays.ShowArrays()
+	arrays.ShowSlices()
+	arrays.ShowSlices2()
+
+	// functions vale items
+	functions.Represent()
+	functions.NamedReturns()
+	functions.ShowAnonfunc()
+	functions.ShowVariadicFunc()
+	functions.ShowFunctions()
+	functions.ShowFuncAsArgument()
+	functions.ShowClosures()
+	functions.ShowMethods()
+
+	// maps vaale items
 	maps.ShowMaps()
 	maps.ModifyMaps()
 	maps.CheckKeyExists()
